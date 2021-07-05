308b010eef516892430784bfb69cca200ea58cd1
Forden <forden@forden.me>
2021-07-05 17:07:44 +0300

    Suppressing exception

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -1,3 +1,5 @@
+from contextlib import suppress
+
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
@@ -8,8 +10,6 @@ import states.user
 async def bot_start(msg: types.Message, state: FSMContext):
     await msg.answer(f'Главное меню', reply_markup=keyboards.default.MainMenu.main_menu())
     if await state.get_state() is not None:
-        try:
+        with suppress():
             await state.finish()
-        except:
-            pass
     await states.user.MainMenu.main_menu.set()
