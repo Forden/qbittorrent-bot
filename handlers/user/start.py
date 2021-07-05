5a1ead49810db8fcbd406dad28f659c51360b93c
Forden <forden@forden.me>
2021-07-05 17:22:10 +0300

    Handling KeyError

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -10,6 +10,6 @@ import states.user
 async def bot_start(msg: types.Message, state: FSMContext):
     await msg.answer(f'Главное меню', reply_markup=keyboards.default.MainMenu.main_menu())
     if await state.get_state() is not None:
-        with suppress():
+        with suppress(KeyError):
             await state.finish()
     await states.user.MainMenu.main_menu.set()
