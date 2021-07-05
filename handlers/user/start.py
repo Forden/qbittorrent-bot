ec36c5252f8d934c1756675d405bb242a534f88a
Forden <forden@forden.me>
2021-07-05 16:53:28 +0300

    Fix situation when user doesn't have state

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -8,5 +8,8 @@ import states.user
 async def bot_start(msg: types.Message, state: FSMContext):
     await msg.answer(f'Привет, {msg.from_user.full_name}!', reply_markup=keyboards.default.MainMenu.main_menu())
     if await state.get_state() is not None:
-        await state.finish()
+        try:
+            await state.finish()
+        except:
+            pass
     await states.user.MainMenu.main_menu.set()
